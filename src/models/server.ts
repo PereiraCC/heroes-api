aaf57c32221447d5f5753190362b7fe0f2f50dc9
Carlos Pereira <carloscoto0103@gmail.com>
2022-01-25 13:08:54 -0600

    Server.ts: add images route

--- src/models/server.ts
+++ src/models/server.ts
@@ -2,13 +2,20 @@ import express, { Application } from 'express';
 import cors from 'cors';
 
 import heroesRoute from '../routes/heroes';
+import imagesRoute from '../routes/images';
+
+type paths = {
+    heroes: string,
+    images: string   
+}
 
 class Server {
 
     private app : Application;
     private port : string;
-    private apiPaths = {
-        heroes : '/api/heroes',
+    private apiPaths : paths = {
+        heroes: '/api/heroes',
+        images: '/api/images'
     }
     
     constructor() {
@@ -27,23 +34,15 @@ class Server {
     }
 
     middlewares() {
-
         this.app.use( cors() );
-
         this.app.use( express.json() );
-
         this.app.use( express.static('public') );
-
     }
 
     routes() {
-        
         this.app.use( this.apiPaths.heroes, heroesRoute);
-
+        this.app.use( this.apiPaths.images, imagesRoute);
     }
-
-
-
 }
 
 export default Server;
